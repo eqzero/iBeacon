9a6d60043f7c412eef22f446c31a999f1694dda0
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-04-22 18:00:07 +0700

    no message

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ noble.on('discover', function(peripheral) {
   var rss = peripheral.rssi;
   var localName = advertisement.localName; 
   console.log('found device: ', macAdress, ' ', localName, ' ', rss);   
-}
+});
