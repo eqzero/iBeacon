62fbadc7349db8911b7ecfb7b0328c5e0c210701
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-04-24 14:29:30 +0700

    no message

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.log('Noble running!!.......');
 
-var socket = require('socket.io-client')('http://127.0.0.1/scanner');
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+  socket.on('event', function(data){
+  	console.log('event...');
+  });
+  socket.on('disconnect', function(){
+  	console.log('disconnect...');
+  });
+});
+server.listen(3000);
