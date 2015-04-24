ec8be15b83cb70c19d47a07ae674427befe0676f
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-04-24 16:46:59 +0700

    no message

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+console.log('Noble running!!.......');
 
-server.listen(3000);
+// var server = require('http').createServer();
 
-app.get('/',function(req,res){
-	res.sendfile(__dirname + '/index.html');
-});
+// var noble = require('noble');
+ 
+// //replace localhost with your server's IP;
+// var socket = require('socket.io-client')('http://10.137.170.59/scanner');
+ 
+// //replace with your hardware address
+// // var addressToTrack = '7c669d9b2dda'; 
+// var addressToTrack = '08:00:27:60:70:62'; 
+ 
+// socket.on('connect', function(){  
+//   console.log('connected to server');
+// });
+ 
+// noble.on('discover', function(peripheral){
+//   if(peripheral.uuid == addressToTrack){
+//     socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});    
+//   }
+// });
+ 
+// noble.startScanning([], true) //allows dubplicates while scanning
 
-io.sockets.on('connection',function(socket){
-	socket.on('send message',function(data){
-		io.sockets.emit('new message',data);
-		
-	});
-});
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// var express = require('express');
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+ 
+// var scanner = io.of('/scanner'); 
+ 
+// scanner.on('connection', function(socket) {
+ 
+//     console.log('Scanner Connected');
+    
+//     socket.on('message', function(msg) {
+//         //recived message from scanner
+//         //do some processing here
+//     });
+ 
+//     socket.on('disconnect', function() {
+//         console.log('Scanner Disconnected');
+//     });
+// });
+ 
+// http.listen(3000, function() {
+//     console.log('listening on *:3000');
+// });
