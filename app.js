f9e419561c71add368d7d8e9387280cb8954823e
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-04-30 17:54:48 +0700

    no message

--- app.js
+++ app.js
@@ -2,48 +2,56 @@ console.log('Noble running!!.......');
 
 var noble = require('noble');
  
-// //replace localhost with your server's IP;
-// var socket = require('socket.io-client')('http://10.137.170.59/scanner');
+//replace localhost with your server's IP;
+var socket = require('socket.io-client')('http://localhost/scanner');
  
-// // replace with your hardware address
-// // var addressToTrack = '7c669d9b2dda'; 
-// var addressToTrack = '08:00:27:60:70:62'; 
+// replace with your hardware address
+// var addressToTrack = '7c669d9b2dda'; 
+var addressToTrack = '08:00:27:60:70:62'; 
  
-// socket.on('connect', function(){  
-//   console.log('connected to server');
-// });
+socket.on('connect', function(){  
+  console.log('connected to server');
+});
  
-// noble.on('discover', function(peripheral){
-//   if(peripheral.uuid == addressToTrack){
-//     socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});    
-//   }
-// });
+noble.on('discover', function(peripheral){
+  if(peripheral.uuid == addressToTrack){
+    socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});    
+  }
+});
  
-// noble.startScanning([], true) //allows dubplicates while scanning
+noble.startScanning([], true) //allows dubplicates while scanning
 
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// var express = require('express');
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
  
-// var scanner = io.of('/scanner'); 
+var scanner = io.of('/scanner'); 
  
-// scanner.on('connection', function(socket) {
+scanner.on('connection', function(socket) {
  
-//     console.log('Scanner Connected');
+    console.log('Scanner Connected');
     
-//     socket.on('message', function(msg) {
-//         //recived message from scanner
-//         //do some processing here
-//     });
+    socket.on('message', function(msg) {
+        //recived message from scanner
+        //do some processing here
+    });
  
-//     socket.on('disconnect', function() {
-//         console.log('Scanner Disconnected');
-//     });
-// });
+    socket.on('disconnect', function() {
+        console.log('Scanner Disconnected');
+    });
+});
  
-// http.listen(3000, function() {
-//     console.log('listening on *:3000');
-// });
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
+
+
+
+
+
+
+// JOEI
