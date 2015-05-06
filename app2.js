643ff5853e5238bb1b574df0ab074188b2eff90e
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-05-06 10:50:44 +0700

    no message

--- app2.js
+++ app2.js
@@ -1,4 +1,6 @@
 var noble = require('noble');
+var rssi_ibeacon = '';
+
 noble.on('stateChange', function(state) {
 	if (state === 'poweredOn' ) {
 		noble.startScanning([], false);
@@ -10,10 +12,15 @@ noble.on('stateChange', function(state) {
 noble.on('discover', function(peripheral) {
 	peripheral.connect(function(error) {});
  
-	peripheral.on('connect',function(){}); 
+	peripheral.on('connect',function(){
+		console.log('connect');
+	}); 
  
 	peripheral.on('rssiUpdate',function(rssi){
-		console.log({"uuid": peripheral.uuid, "rssi": peripheral.rssi });
+		if(rssi_ibeacon != peripheral.rssi){
+			rssi_ibeacon = peripheral.rssi;
+			console.log({"uuid": peripheral.uuid, "rssi": peripheral.rssi });
+		}
 	});
  
 	setInterval(function(){
