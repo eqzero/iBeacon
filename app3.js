c2f173d4d7f931ca1e05aa583e59f9ab33094fc6
unknown <apichart.a@BKKMITNTB6771.ad.insidemedia.net>
2015-05-06 11:48:19 +0700

    no message

--- app3.js
+++ app3.js
@@ -20,7 +20,11 @@ noble.on('discover', function(peripheral) {
   console.log('\t\t' + JSON.stringify(peripheral.advertisement.serviceUuids));
   */
 
-  console.log('rssi: '+peripheral.rssi);
+  // console.log('rssi: '+peripheral.rssi);
+
+  var rssi_new = calculateDistance(peripheral.rssi);
+
+  console.log(rssi_new);
   /*
   var serviceData = peripheral.advertisement.serviceData;
   if (serviceData && serviceData.length) {
@@ -40,3 +44,21 @@ noble.on('discover', function(peripheral) {
   */
   console.log('<-------------->');
 });
+
+function calculateDistance(rssi) {
+  
+  var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
+  
+  if (rssi == 0) {
+    return -1.0; 
+  }
+ 
+  var ratio = rssi*1.0/txPower;
+  if (ratio < 1.0) {
+    return Math.pow(ratio,10);
+  }
+  else {
+    var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
+    return distance;
+  }
+} 
